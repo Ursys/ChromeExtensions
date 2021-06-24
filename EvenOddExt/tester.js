aa6e427a6ba1d843eeb3dc4241780eb8eb2545a6
Ursys <jeb.mccluskey@gmail.com>
2021-06-24 16:03:43 -0400

    Adding pair of Ts extension
    This was the first extension I created. It is a simple parity function to test if a number is even or odd

---
+++ EvenOddExt/tester.js
@@ -0,0 +1,21 @@
+numberTest();
+
+function numberTest () {
+  var testButton = document.getElementById("test");
+  testButton.onclick = function() {
+    evenOdd();
+  };
+}
+
+function evenOdd() {
+  var input = document.getElementById("userInput").value;
+  if (isNaN(input)) {
+    document.getElementById("result").innerHTML = input + " is not a number. Please enter a number";
+  } else {
+    if (input % 2 === 0) {
+      document.getElementById("result").innerHTML = input + " is an EVEN number";
+    } else {
+      document.getElementById("result").innerHTML = input + " is an ODD number";
+    }
+  }
+}
