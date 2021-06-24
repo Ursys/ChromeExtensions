fbe9c055426f0bca2b0770a07d86ab11564f6bdc
Ursys <jeb.mccluskey@gmail.com>
2021-06-24 16:01:33 -0400

    BSI Fixer Extension
    Adding the BSI Fixer Extension. This extension removes the stuff that clogs up the BSI compliance nav interface.

---
+++ FixBSI/content.js
@@ -0,0 +1,26 @@
+console.log('Starting to fix');
+
+/*	var elem = document.getElementsByClassName('content-header');
+console.log(elem);
+
+console.log('start delete');
+var elem = document.getElementsByClassName('content-header');
+console.log(elem[0]);
+elem[0].parentNode.removeChild(elem[0]);
+
+console.log('Bye Felicia');
+
+
+function removeHeader() 
+{
+	if (document.getElementsById("item1").checked)
+	{
+		var newStyle = document.createElement('style');
+		style.innerHTML = 
+			'.content-header {' +
+				'display: none !important;' +
+			'}';
+	}
+}
+*/
+
